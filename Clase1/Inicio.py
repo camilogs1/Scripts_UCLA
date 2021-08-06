49d9e4aea6671062529873bfbd4d47a3e35338ba
Camilo García <72902253+camilogs1@users.noreply.github.com>
2021-08-06 16:49:24 -0500

    Ejercicio_clase_1

---
+++ Clase1/Inicio.py
@@ -0,0 +1,14 @@
+kilo=12.25
+kilo_millas=kilo/1.6
+millas=7.38
+millas_kilo=millas*1.6
+print(kilo, " kilometros son: ", kilo_millas, "millas")
+print(millas, " millas son: ", millas_kilo, " kilometros")
+
+x=float(input("Ingrese el valor de x: "))
+y=float(input("\nIngrese el valor de y: "))
+
+hipo=(x**2 + y**2)**1/2
+
+print("\nla hipotenusa es:", hipo)
+
