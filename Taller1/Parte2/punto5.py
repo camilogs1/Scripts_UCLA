85afe88f9d25f975c017bfe3c44ce50c43ededac
Bryan_Arias <bryanariasq02@gmail.com>
2021-08-25 12:58:29 -0500

    Update punto5.py

--- Taller1/Parte2/punto5.py
+++ Taller1/Parte2/punto5.py
@@ -1,11 +1,13 @@
-x= float(input("\tIntroducir dos numeros para relaizar operaciones\nNumero 1: "))
-y= float(input("Numero 2: "))
+x= int(input("\tIntroducir dos numeros para relaizar operaciones\nNumero 1: "))
+y= int(input("Numero 2: "))
 
 suma = x+y
 resta = x-y
 mult = x*y
 divi = x/y
 pot = x**y
+xbin = bin(x)
+ybin = bin(y)
 derx = x>>1
 dery = y>>1
 izqx = x<<1
@@ -16,5 +18,9 @@ print("La resta de {0} y {1} es: {2}".format(x,y,resta))
 print("La multiplicación de {0} y {1} es: {2}".format(x,y,mult))    
 print("La división de {0} y {1} es: {2}".format(x,y,divi))
 print("La potencia de {0} a la {1} es: {2}".format(x,y,pot))    
-
+print("El desplazamiento de x a la izquierda y derecha son {} y {} respectivamente"
+      .format(izqx, derx))
+print("El desplazamiento de y a la izquierda y derecha son {} y {} respectivamente"
+      .format(izqy, dery))
+print()
 ###  INCOMPLETO
