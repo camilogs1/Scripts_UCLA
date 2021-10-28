77cab46a5ef0967665c561d249df7252c44ae409
Camilo García <72902253+camilogs1@users.noreply.github.com>
2021-10-28 17:18:15 -0500

    Update graficas.py

--- ProyectoFinal/graficas.py
+++ ProyectoFinal/graficas.py
@@ -1,34 +1,38 @@
+from mpl_toolkits.mplot3d import axes3d
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-ucla = pd.read_csv(r"C:\Users\bryan\Desktop\Universidad\2021-2\Python\Scripts_UCLA\ProyectoFinal\ucla.csv", sep=",")
+#llamado datos
+ucla = pd.read_csv("https://raw.githubusercontent.com/camilogs1/Scripts_UCLA/main/ProyectoFinal/ucla.csv", sep=",")
 
+#organizar datos
 cantidad = ucla[["grupo","ano"]]
 cantidad2 = cantidad.groupby(["grupo"]).count()
 cantidad2.ano.astype(float)
-
 cantidad = ucla[["grupo","ano"]]
 cantidad3 = cantidad.groupby(["ano"]).count()
-#1
+cantidad4 = ucla[["grupo", "ano", "vol"]]
+
+#punto 1
 plt.stem(cantidad3.index, cantidad3["grupo"])
-#2
+#punto 2
 plt.plot(cantidad3.index, cantidad3["grupo"])
-#3
+#punto 3
 plt.pie(cantidad2.ano, labels=cantidad2.index)
-#4
+#punto 4
 plt.scatter(cantidad3.index, cantidad3["grupo"], c="red")
-#5
+#punto 5
 plt.boxplot(cantidad3["grupo"])
-#6
+#punto 6
 plt.bar(cantidad3.index, cantidad3["grupo"])
-#7
+#punto 7
 plt.barh(cantidad2.index, cantidad2.ano)
-#8
+#punto 8
 plt.step(cantidad3.index, cantidad3["grupo"])
-#9
+#punto 9
 plt.hist(cantidad3["grupo"], edgecolor="red")
-#10
+#punto 10
 fig, ax= plt.subplots(3)
 fig.suptitle=('datos UCLA')
 ax[0].plot(cantidad3.index, cantidad3["grupo"])
@@ -41,4 +45,19 @@ ax[0].plot(cantidad3.index, cantidad3["grupo"])
 ax[1].barh(cantidad2.index, cantidad2.ano)
 ax[2].scatter(cantidad3.index, cantidad3["grupo"], c="red")
 plt.show()
-#11
+#punto 11
+fig = plt.bar(cantidad3.index, cantidad3["grupo"])
+fig = plt.title("Grafica grupos UCLA")
+fig = plt.xlabel('año')
+fig = plt.ylabel('articulos')
+fig = plt.grid(True)
+fig = plt.legend(['Articulos por año'])
+fig = plt.figure(figsize=(8,6))
+#punto 12 con datos aleatorios ya que no poseemos con los datos necesarios para darle forma a la grafica
+x=np.random.randint(10, size=60)
+y=np.random.randint(10, size=60)
+z=np.random.randint(10, size=60)
+axes = plt.axes(projection="3d")
+axes.scatter3D(x,y,z,color="blue")
+axes.set_title("Grafica en 3d")
+#punto 13
